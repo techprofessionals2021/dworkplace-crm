953ba0626106a1f9f1377bf73ef977eadce63624
syedhammadali1 <hammadali407292@gmail.com>
2024-09-03 20:32:02 +0500

    soft delete

--- app/Models/User.php
+++ app/Models/User.php
@@ -14,9 +14,11 @@ use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
 use App\Notifications\CustomResetPasswordNotification;
 use Illuminate\Database\Eloquent\Relations\BelongsToMany;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
 class User extends Authenticatable
 {
-    use HasApiTokens, HasFactory, Notifiable;
+    use HasApiTokens, HasFactory, Notifiable, SoftDeletes;
 
     /**
      * The attributes that are mass assignable.

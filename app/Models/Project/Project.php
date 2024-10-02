db516066a67047415970d5fb7715cabdcc18e738
syedhammadali1 <hammadali407292@gmail.com>
2024-10-02 21:10:37 +0500

    .

--- app/Models/Project/Project.php
+++ app/Models/Project/Project.php
@@ -6,16 +6,27 @@ namespace App\Models\Project;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use App\Models\Project\Relationship\ProjectRelationship;
 use Illuminate\Database\Eloquent\Model;
+use Spatie\Activitylog\LogOptions;
+use Spatie\Activitylog\Traits\LogsActivity;
 use Spatie\MediaLibrary\HasMedia;
 use Spatie\MediaLibrary\InteractsWithMedia;
 
 class Project extends Model implements HasMedia
 {
-    use HasFactory, ProjectRelationship, InteractsWithMedia;
+    use HasFactory, ProjectRelationship, InteractsWithMedia, LogsActivity;
 
     protected $fillable = [
         'creator_id', 'project_code', 'sales_code', 'title', 'description',
         'client_id', 'source_account_id', 'status_id', 'deadline',
     ];
+
+    public function getActivitylogOptions(): LogOptions
+    {
+        return LogOptions::defaults()
+            ->logOnly(['title', 'description', 'status_id', 'deadline'])
+            ->logOnlyDirty()
+            ->useLogName('project')
+            ->setDescriptionForEvent(fn(string $eventName) => "Project {$eventName} by " . auth()->user()->name);
+    }
 
 }

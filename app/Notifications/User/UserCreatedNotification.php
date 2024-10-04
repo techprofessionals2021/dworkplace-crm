3f5f5ebe2fab8fe087d865d8cb4f9094d3280fb1
syedhammadali1 <hammadali407292@gmail.com>
2024-10-04 21:27:46 +0500

    notifications + broad casting

---
+++ app/Notifications/User/UserCreatedNotification.php
@@ -0,0 +1,76 @@
+<?php
+
+namespace App\Notifications\User;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Notifications\Messages\BroadcastMessage;
+use Illuminate\Notifications\Messages\MailMessage;
+use Illuminate\Notifications\Notification;
+
+class UserCreatedNotification extends Notification
+{
+    use Queueable;
+
+    public $user;
+    /**
+     * Create a new notification instance.
+     */
+    public function __construct($user)
+    {
+        $this->user = $user;
+    }
+
+    /**
+     * Get the notification's delivery channels.
+     *
+     * @return array<int, string>
+     */
+    public function via(object $notifiable): array
+    {
+        return ['database', 'broadcast'];
+    }
+
+    /**
+     * Get the mail representation of the notification.
+     */
+    public function toMail(object $notifiable): MailMessage
+    {
+        return (new MailMessage)
+                    ->line('The introduction to the notification.')
+                    ->action('Notification Action', url('/'))
+                    ->line('Thank you for using our application!');
+    }
+
+
+
+    public function toDatabase($notifiable)
+    {
+        return [
+            'message' => 'User "' . $this->user->name . '" has been created.',
+            'user_id' => $this->user->id,
+        ];
+    }
+
+
+    public function toBroadcast($notifiable)
+    {
+        return new BroadcastMessage([
+            'message' => 'User "' . $this->user->name . '" has been created.',
+            'user_id' => $this->user->id,
+        ]);
+    }
+
+
+    /**
+     * Get the array representation of the notification.
+     *
+     * @return array<string, mixed>
+     */
+    public function toArray(object $notifiable): array
+    {
+        return [
+            //
+        ];
+    }
+}
